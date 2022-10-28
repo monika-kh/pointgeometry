7db8f5a984789eff36948e166413e035d7811d2c
monika-kh <58970310+monika-kh@users.noreply.github.com>
2022-10-28 19:09:53 +0530

    Update local_settings.py

--- kartozaportfolio/local_settings.py
+++ kartozaportfolio/local_settings.py
@@ -1,7 +1,7 @@
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': "d7uno6qfomc64d",
         'USER': "vqutjzrydnnoic",
         'PASSWORD': "fff1b5d5c07203134805fffe99793a0a8ac96e021666514bd0a7e1dc73016c6d",
