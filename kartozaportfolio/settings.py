dd6769ed8c80292f9d50af29cd57df117b4b9554
monika-kh <58970310+monika-kh@users.noreply.github.com>
2022-10-28 15:07:27 +0530

    Update settings.py

--- kartozaportfolio/settings.py
+++ kartozaportfolio/settings.py
@@ -14,6 +14,7 @@ from logging import Logger
 from pathlib import Path
 import environ
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
