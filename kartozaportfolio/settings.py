a4655e57755efa6c77639ce0e36a338dcc2d05e9
monika-kh <58970310+monika-kh@users.noreply.github.com>
2022-10-28 18:52:39 +0530

    Update settings.py

--- kartozaportfolio/settings.py
+++ kartozaportfolio/settings.py
@@ -172,11 +172,23 @@ try:
     if DEBUG == True:
         try:
 #             from .local_settings import *
-              DATABASES = {'default': dj_database_url.config()}
+#               DATABASES = {'default': dj_database_url.config()}
+               DATABASES = {
+                'default': {
+                    'ENGINE': 'django.contrib.gis.db.backends.postgis',
+                    'NAME': 'db',
+                    }
+                }
         except ImportError:
             Logger.info("Import local.settings file")
     else:
-        DATABASES = {'default': dj_database_url.config()}
+#         DATABASES = {'default': dj_database_url.config()}
+          DATABASES = {
+            'default': {
+                'ENGINE': 'django.contrib.gis.db.backends.postgis',
+                'NAME': 'ciba',
+            }
+        }
         
 #         DATABASES = {
 #             'default': {
